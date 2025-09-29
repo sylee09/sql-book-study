f3cbe97ed707bfc11b3f61afcb3ac81e35e32e48
Seungyoung Lee <leesy960909@gmail.com>
2025-09-29 21:33:08 +0900

    p9-1번 문제풀이

---
+++ p9/p9-1.sql
@@ -0,0 +1,4 @@
+select machine_id, 
+round(sum(case when activity_type = 'end' then timestamp else -timestamp end)/count(distinct process_id),3) as processing_time
+from activity
+group by machine_id
